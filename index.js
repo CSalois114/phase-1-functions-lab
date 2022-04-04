72607f617bc011ae6a490b7dd5e98c11d23abb82
Chris Salois <csalois@gmail.com>
2022-04-04 12:11:48 -0400

    commit tree 769946e6bc73b1df2cb75b90c7187f7ff41f59c1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (block) {
+  const scuberHqBlock = 42;
+  return Math.abs(scuberHqBlock - block);
+}
+
+function distanceFromHqInFeet (block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if(distance <= 400) return 0;
+  if(distance < 2000) return (distance - 400) * .02;
+  if(distance < 2500) return 25;
+  return 'cannot travel that far';
+}
